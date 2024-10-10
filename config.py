6c24b195260ce0eeb9b7165312c8642fe204529b
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-10-10 14:13:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,12 +32,12 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "1"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link."-1002445901197)
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
