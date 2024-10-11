b820dca88011787a0a2295627a8da34025a46606
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-10-11 12:36:06 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Channel : @PXL_Official\n○ Movies Channel : @PXL_MOVIES\n○ PFP Channel : @PXL_Pfp</b>",
+            text = f"<b>○ Channel : @PXL_Official\n○ Anime Channel : @PXL_ANIME\n○ Movies Channel : @PXL_MOVIES\n○ PFP Channel : @PXL_Pfp</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
