c59b13cdfdecbd56c895c4dfee89dadd93f2b116
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-10-12 12:12:56 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,8 +8,7 @@ import asyncio
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from info import AUTH_CHANNEL
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import *
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
 from bot import Bot
