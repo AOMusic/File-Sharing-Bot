bc83f60c5dad8479819f62c303e5a1e92fd5eb15
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-10-21 09:43:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,10 +10,6 @@ from logging.handlers import RotatingFileHandler
 
 
 
-id_pattern = re.compile(r'^.\d+$')
-
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '0').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
-
 #Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
